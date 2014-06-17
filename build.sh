c205c1f8e49e35c90e622ebf25627bc14baf5824
Andrew Dolgert <adolgert@cornell.edu>
2014-06-17 16:37:23 -0400

    adding an autoreconf

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ mkdir /home/jenkins
 cd /home/jenkins
 git clone https://github.com/afidd/Semi-Markov.git
 cd Semi-Markov
+echo "autoreconf now"
+autoreconf --force --reinstall
+echo "done autoreconf"
 ./configure
 make
